334c087a550a3de707d5475621b4ac4843e7d6e0
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2023-04-24 12:16:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# malva
+
+# How to install
+```
+sudo dpkg -i <(curl -s https://github.com/HyperloopUPV-H8/malva/releases/download/v0.1.0/malva_0.1-1_amd64.deb)
+```
---
+++ malva_0.1-1_amd64/DEBIAN/postinst.sh
@@ -0,0 +1,5 @@
+s ln -s /usr/share/arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc /usr/local/bin/ 
+s ln -s /usr/share/arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-g++ /usr/local/bin/ 
+s ln -s /usr/share/arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-objcopy /usr/local/bin/ 
+s ln -s /usr/share/arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-nm /usr/local/bin/ 
+s ln -s /usr/share/arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-size /usr/local/bin/ 
